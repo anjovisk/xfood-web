119e5f83fd8f7420be7872b8efd1f5d1f9588d10
anjovisk <anjovisk@gmail.com>
2021-03-24 18:23:45 -0300

    Refatorar componente para habilitar/dasabilitar pratos
    Criar um componente único responsável por habilitar/desabilitar os pratos; refatorar card de pratos.

---
+++ src/components/adm/ChangeFoodStatusButton.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { Button, Spinner } from "react-bootstrap";
+
+export const ChangeFoodStatusButton = (props) => {
+    const [isChangingStatus, setChangingStatus] = useState(false);
+
+    const changeFoodStatus = (food) => {
+        setChangingStatus(true);
+        const status = food.status.value === "available"
+            ? { value: "unavailable", name: "Unavailable" }
+            : { value: "available", name: "Available" };
+        
+        fetch(`http://localhost:3001/public/v1/foods/${food.id}/status`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(status)
+        }).then(
+            res => {
+                if (res.status === 200) {
+                    return res.json();
+                }
+                switch (res.status) {
+                    case 200:
+                        return res.json();
+                    case 404:
+                        throw Error("Food status could not be changed. Food not found.");
+                    default:
+                        throw Error("Food status could not be changed.");
+                }
+            }
+        ).then(
+            (result) => {
+                if (props.onFoodStatusChanged) {
+                    props.onFoodStatusChanged(food.id, result.status);
+                }
+                setChangingStatus(false);
+            },
+            (error) => {
+                setChangingStatus(false);
+                if (props.onError) {
+                    props.onError( { title: food.name, message: error.message });
+                }
+            }
+        );
+    }
+
+    const loadingSpinner = (
+        <>
+            <Spinner
+                as="span"
+                animation="border"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+            />
+                <span className="sr-only">Loading...</span>
+            <Spinner/>
+        </>
+    );
+
+    return (
+        <Button style={{minWidth: "150px"}} 
+            onClick={!isChangingStatus ? () => changeFoodStatus(props.food) : null } 
+            disabled={isChangingStatus}>
+            {isChangingStatus 
+                ? loadingSpinner 
+                : props.food.status.value === "available" ? "Turn unavailable" : "Turn available" 
+            }
+        </Button>
+    );
+}
+
+export default ChangeFoodStatusButton;
--- src/components/adm/FoodCardFooter.jsx
+++ src/components/adm/FoodCardFooter.jsx
@@ -1,76 +1,24 @@
 import { useState } from "react";
 import { Button, ButtonGroup, Card, Modal, OverlayTrigger, Popover, Spinner } from "react-bootstrap";
+import ChangeFoodStatusButton from './ChangeFoodStatusButton';
 
 export const FoodCardFooter = (props) => {
     const [error, setError] = useState(null);
-    const [isChangingStatus, setChangingStatus] = useState(false);
 
-    const changeFoodStatus = (food) => {
-        setChangingStatus(true);
-        const status = food.status.value === "available"
-            ? { value: "unavailable", name: "Unavailable" }
-            : { value: "available", name: "Available" };
-        
-        fetch(`http://localhost:3001/public/v1/foods/${food.id}/status`, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(status)
-        }).then(
-            res => {
-                if (res.status === 200) {
-                    return res.json();
-                }
-                switch (res.status) {
-                    case 200:
-                        return res.json();
-                    case 404:
-                        throw Error("Food status could not be changed. Food not found.");
-                    default:
-                        throw Error("Food status could not be changed.");
-                }
-            }
-        ).then(
-            (result) => {
-                props.onFoodStatusChanged(food.id, result.status);
-                setChangingStatus(false);
-            },
-            (error) => {
-                setChangingStatus(false);
-                setError( { title: food.name, message: error.message });
-            }
-        );
-    }
-
-    const loadingSpinner = (
-        <>
-            <Spinner
-                as="span"
-                animation="border"
-                size="sm"
-                role="status"
-                aria-hidden="true"
-            />
-                <span className="sr-only">Loading...</span>
-            <Spinner/>
-        </>
-    );
+    const onChangeFoodStatusError = (error) => {
+        setError(error);
+    };
 
     const popover = (
         <Popover id="popover-food-settings">
             <Popover.Title as="h3">{props.food.name}</Popover.Title>
             <Popover.Content>
                 <ButtonGroup vertical>
-                    <Button style={{minWidth: "150px"}} 
-                        onClick={!isChangingStatus ? () => changeFoodStatus(props.food) : null } 
-                        disabled={isChangingStatus}>
-                        {isChangingStatus 
-                            ? loadingSpinner 
-                            : props.food.status.value === "available" ? "Turn unavailable" : "Turn available" 
-                        }
-                    </Button>
+                    <ChangeFoodStatusButton
+                        onFoodStatusChanged={props.onFoodStatusChanged}
+                        food={props.food}
+                        onError={onChangeFoodStatusError}
+                    />
                     <Button>Change</Button>
                     <Button>Remove</Button>
                 </ButtonGroup>
